ef5109e91747dfb919232584058e04e984970cdf
raoulbia <raoulbia@gmail.com>
2019-11-06 14:10:07 +0000

    update

--- Tutorials/Efficient Frontier with Quandl (Part 1).py
+++ Tutorials/Efficient Frontier with Quandl (Part 1).py
@@ -5,8 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # get adjusted closing prices of 5 selected companies with Quandl
-quandl.ApiConfig.api_key = 'zcfJ6696mcZScjzsyeta'
+quandl.ApiConfig.api_key = '6sHkfEoiqR4ykcsQkSnh'
 selected = ['CNP', 'F', 'WMT', 'GE', 'TSLA']
+selected = ['CNP', 'F']
+selected_ls = [['CNP', 'F'], ['WMT', 'GE']]
 data = quandl.get_table('WIKI/PRICES', ticker = selected,
                         qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
                         date = { 'gte': '2014-1-1', 'lte': '2016-12-31' }, paginate=True)
@@ -15,14 +17,24 @@ data = quandl.get_table('WIKI/PRICES', ticker = selected,
 # columns of tickers and their corresponding adjusted prices
 clean = data.set_index('date')
 table = clean.pivot(columns='ticker')
+# print(table.head())
 
-# calculate daily and annual returns of the stocks
+# calculate daily returns of the stocks (looks similar to basis point estimates)
 returns_daily = table.pct_change()
-returns_annual = returns_daily.mean() * 250
+# print(returns_daily.head())
+
+# calculate annual returns of the stocks
+returns_annual = returns_daily.mean() * 250 # looks similar to mu_values
+returns_annual = returns_annual.to_numpy()
+# print(returns_annual)
+# print(returns_annual.shape)
 
 # get daily and covariance of returns of the stock
 cov_daily = returns_daily.cov()
+# print(cov_daily.shape)
+# print(cov_daily)
 cov_annual = cov_daily * 250
+# print(cov_annual)
 
 # empty lists to store returns, volatility and weights of imiginary portfolios
 port_returns = []
@@ -30,35 +42,38 @@ port_volatility = []
 stock_weights = []
 
 # set the number of combinations for imaginary portfolios
-num_assets = len(selected)
-num_portfolios = 50000
+for selected in selected_ls:
+    num_assets = len(selected)
+    num_portfolios = 50000
 
-# populate the empty lists with each portfolios returns,risk and weights
-for single_portfolio in range(num_portfolios):
-    weights = np.random.random(num_assets)
-    weights /= np.sum(weights)
-    returns = np.dot(weights, returns_annual)
-    volatility = np.sqrt(np.dot(weights.T, np.dot(cov_annual, weights)))
-    port_returns.append(returns)
-    port_volatility.append(volatility)
-    stock_weights.append(weights)
+    # populate the empty lists with each portfolios returns,risk and weights
+    for single_portfolio in range(num_portfolios):
+        weights = np.random.random(num_assets)
+        weights /= np.sum(weights)
+        returns = np.dot(weights, returns_annual)
+        volatility = np.sqrt(np.dot(weights.T, np.dot(cov_annual, weights)))
+        port_returns.append(returns)
+        port_volatility.append(volatility)
+        stock_weights.append(weights)
 
-# a dictionary for Returns and Risk values of each portfolio
-portfolio = {'Returns': port_returns,
-             'Volatility': port_volatility}
+    # a dictionary for Returns and Risk values of each portfolio
+    portfolio = {'Returns': port_returns,
+                 'Volatility': port_volatility}
 
-# extend original dictionary to accomodate each ticker and weight in the portfolio
-for counter,symbol in enumerate(selected):
-    portfolio[symbol+' Weight'] = [Weight[counter] for Weight in stock_weights]
+    # extend original dictionary to accomodate each ticker and weight in the portfolio
+    for counter,symbol in enumerate(selected):
+        portfolio[symbol+' Weight'] = [Weight[counter] for Weight in stock_weights]
 
-# make a nice dataframe of the extended dictionary
-df = pd.DataFrame(portfolio)
+    # make a nice dataframe of the extended dictionary
+    df = pd.DataFrame(portfolio)
 
-# get better labels for desired arrangement of columns
-column_order = ['Returns', 'Volatility'] + [stock+' Weight' for stock in selected]
+    # get better labels for desired arrangement of columns
+    column_order = ['Returns', 'Volatility'] + [stock+' Weight' for stock in selected]
 
-# reorder dataframe columns
-df = df[column_order]
+    # reorder dataframe columns
+    df = df[column_order]
+    print(df.head())
+    break
 
 # plot the efficient frontier with a scatter plot
 plt.style.use('seaborn')
